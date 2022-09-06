9e650aeeddd9a32139d2ffb661c2e7f97612624a
mario mendonca <mariomeendonca00@gmail.com>
2022-09-06 18:06:23 -0300

    feat: create, get all and get by id posts functions

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -14,11 +14,24 @@ export const resolvers = {
       })
 
       return user
+    },
+
+    posts: async () => {
+      const posts = await prisma.post.findMany()
+      return posts
+    },
+    post: async (_: any, { id }: { id: string }) => {
+      const post = await prisma.post.findUnique({
+        where: {
+          id
+        }
+      })
+      return post
     }
   },
 
   Mutation: {
-    create: async (_: any, { email, bio, password }: { email: string, bio: string, password: string }) => {
+    createUser: async (_: any, { email, bio, password }: { email: string, bio: string, password: string }) => {
       const user = await prisma.user.create({
         data: {
           email,
@@ -29,14 +42,29 @@ export const resolvers = {
 
       return user
     },
-    delete: async (_: any, { id }: { id: string }) => {
+    deleteUser: async (_: any, { id }: { id: string }) => {
       await prisma.user.delete({
         where: {
           id
-        }
+        },
       })
 
       return 'User deleted successfully'
+    },
+
+    createPost: async (_: any, { userId, title, content }: any) => {
+      const post = await prisma.post.create({
+        data: {
+          title,
+          content,
+          userId
+        },
+        include: {
+          User: true
+        }
+      })
+
+      return post
     }
   }
 }
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -6,16 +6,29 @@ export const typeDefs = gql`
     email: String
     bio: String
     password: String
+    posts: [Post]
+    createdAt: String
+  }
+
+  type Post {
+    id: ID!
+    User: User
+    title: String
+    content: String
     createdAt: String
   }
 
   type Query {
     users: [User]
     user(id: ID!): User
+    posts: [Post]
+    post(id: ID!): Post
   }
 
   type Mutation {
-    create(email: String, bio: String, password: String): User
-    delete(id: ID!): String
+    createUser(email: String, bio: String, password: String): User
+    deleteUser(id: ID!): String
+    createPost(userId: ID!, title: String, content: String): Post
+    deletePost(id: ID!): String
   }
 `
