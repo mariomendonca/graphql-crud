10e025f9e0b354b1e16d4f860beadce21f78c0b3
mario mendonca <mariomeendonca00@gmail.com>
2022-09-08 12:32:10 -0300

    monorepo

--- prisma/migrations/20220906195525_create_posts/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "posts" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "userId" TEXT,
-    CONSTRAINT "posts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
